e1b8f0c042af804b31afa9659178e85ef45ed857
orgross <orgross123@gmail.com>
2024-02-19 19:05:44 +0200

    19.2

---
+++ HACKERU/HackerU course 2 2023-2024/C#/FileSystemPart2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json;
+
+namespace FileSystemPart2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string jsonString = File.ReadAllText("students.json");
+            Students students = JsonSerializer.Deserialize<Students>(jsonString);
+            
+            foreach (Student student in students.students)
+            {
+                Console.WriteLine(student.id);
+                Console.WriteLine(student.name);
+                Console.WriteLine(student.description);
+            }
+        }
+    }
+}
---
+++ HACKERU/HackerU course 2 2023-2024/C#/WorkingWithSensetivaData/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace WorkingWithSensetivaData
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var config=new ConfigurationBuilder().AddUserSecrets<Program>().Build();
+
+            string apikey = config["apikey"];
+            Console.WriteLine("the apikey is " + apikey);
+
+            string secret = Environment.GetEnvironmentVariable("secret");
+            Console.WriteLine("the apikey is " + secret);
+
+        }
+    }
+}
